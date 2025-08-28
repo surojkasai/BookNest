9071f90a566b9d0da2f36bde5f65514b52e10a29
surojkasai <surojkasai@gmail.com>
2025-08-28 13:30:43 +0545

    Fix base href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -98,7 +98,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b4d3a91af621a7862edd8209f882dfcf
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "34b1256283581042dfb9293724a41983",
+"flutter_bootstrap.js": "fd9e298a6a57ea771dfdd4f2f70761b4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
